620aa24b9dea7cdd8d2c05ef3fa768769ae942a1
Felix Łubiński <30995@s.pm.szczecin.pl>
2024-10-17 15:49:18 +0200

    Improved responsiveness of layout

--- soundnest-src/src/renderer/topbar.tsx
+++ soundnest-src/src/renderer/topbar.tsx
@@ -1,10 +1,11 @@
-import logo from "../../assets/logo-title.png"
+/* eslint-disable jsx-a11y/alt-text */
+import logo from '../../assets/logo-title.png';
 
 function TopBar() {
   return (
     <div className="topBarContent">
       <div className="iconDiv">
-        <img src={logo}></img>
+        <img src={logo} height="60px" />
         <p>SoundNest</p>
       </div>
       <div className="searchInputDiv">
@@ -12,7 +13,11 @@ function TopBar() {
       </div>
       <div className="profileDiv">
         <div className="walletValueDiv">
-          {sessionStorage.getItem("id") === null ? <p>6,9$</p> : <p>{sessionStorage.getItem("credits")}$</p>  }
+          {sessionStorage.getItem('id') === null ? (
+            <p>6,9$</p>
+          ) : (
+            <p>{sessionStorage.getItem('credits')}$</p>
+          )}
         </div>
         <div className="profileButtonDiv">
           <input type="button" className="profileButton" />
